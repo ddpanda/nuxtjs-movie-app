137237958c1a4d034300054b7520596f0112cf9f
Sarkhan Rajabov <serxanrecebov96@gmail.com>
2020-05-17 21:21:35 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# NuxtJS Movie APP
+
+> My grand Nuxt.js project
+
+DEMO: https://nuxtjs-movie-app.herokuapp.com/
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ components/common/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="header">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <nuxt-link class="navbar-brand" to="/">Movie</nuxt-link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <nuxt-link class="nav-item" tag="li" to="/" exact>
+                            <a class="nav-link" href="#">Home</a>
+                        </nuxt-link>
+                        <nuxt-link class="nav-item" tag="li" to="/movies/top-rated">
+                            <a class="nav-link" href="#">Top Rated</a>
+                        </nuxt-link>
+                        <nuxt-link class="nav-item" tag="li" to="/movies/now-playing">
+                            <a class="nav-link" href="#">Now Playing</a>
+                        </nuxt-link>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+    }
+</script>
+
+<style scoped>
+
+</style>
